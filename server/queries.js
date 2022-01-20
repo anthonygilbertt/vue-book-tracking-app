747948d1ad3d2b3b502f9a738d774120dfbdcb6c
Anthony <anthony.gilbert93@gmail.com>
2022-01-19 20:26:10 -0600

    Logging front end data into node console

--- server/queries.js
+++ server/queries.js
@@ -29,35 +29,35 @@ const getStatusesById = (request, response) => {
     })
 }
 const getBooks = (request, response) => {
-    // const id = parseInt(request.params.id)
-    // pool.query('SELECT * FROM book_status WHERE id = $1', [id], (error, results) => {
-    // pool.query('SELECT * FROM book_status', [id], (error, results) => {
-    // pool.query('SELECT id FROM book_status;', (error, results) => {
-    pool.query('SELECT * FROM book;', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-const createBook = (request, response) => {
-    pool.query(
-        "SELECT setval('book_id_seq', max(id)) FROM book;"
-    )
-    pool.query(
-        "INSERT INTO book(title, author, is_favorite)VALUES('Book Title Test1', 'author test1', false)",
-        (err, res) => {
-            console.log(err, res);
-            // pool.end();
-        }
-    );
-}
+        // const id = parseInt(request.params.id)
+        // pool.query('SELECT * FROM book_status WHERE id = $1', [id], (error, results) => {
+        // pool.query('SELECT * FROM book_status', [id], (error, results) => {
+        // pool.query('SELECT id FROM book_status;', (error, results) => {
+        pool.query('SELECT * FROM book;', (error, results) => {
+            if (error) {
+                throw error
+            }
+            response.status(200).json(results.rows)
+        })
+    }
+    // const createBook = (request, response) => {
+    //     pool.query(
+    //         "SELECT setval('book_id_seq', max(id)) FROM book;"
+    //     )
+    //     pool.query(
+    //         "INSERT INTO book(title, author, is_favorite)VALUES('Book Title Test1', 'author test1', false)",
+    //         (err, res) => {
+    //             console.log(err, res);
+    //             // pool.end();
+    //         }
+    //     );
+    // }
 
 module.exports = {
     getStatuses,
     getStatusesById,
     getBooks,
-    createBook
+    // createBook
     // createUser,
     // updateUser,
     // deleteUser,
--- server/server.js
+++ server/server.js
@@ -6,7 +6,6 @@ const db = require('./queries')
 const cors = require('cors');
 require('dotenv').config()
 
-
 app.use(bodyParser.json())
 app.use(
     bodyParser.urlencoded({
@@ -15,15 +14,23 @@ app.use(
 )
 app.use(cors());
 
-
 app.get('/', (request, response) => {
     response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
+// app.post('/results', (request, response) => {
+app.post('/posts', (request, response) => {
+    // response.json('request: ', request)
+    // response.json('response: ', response)
+    console.log('request: ', request);
+    // response.status(200).json('bloop')
+    // response.json.parse(request)
 })
 
 app.get('/getStatuses', db.getStatuses)
 app.get('/getStatusesById', db.getStatusesById)
 app.get('/getBooks', db.getBooks)
-app.post('/createBook', db.createBook)
+    // app.post('/createBook', db.createBook)
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
