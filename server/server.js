b4ae55f88a35f476b1945999754b493cc265c980
anthonygilbert <anthony.gilbert93@gmail.com>
2022-02-12 01:02:36 -0600

    pass the first stage

--- server/server.js
+++ server/server.js
@@ -1,83 +1,84 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
-const port = 3000
-const db = require('./queries')
-const cors = require('cors');
-const bcrypt = require('bcrypt')
-const flash = require('express-flash')
-const session = require('express-session')
-const initializePassport = require('./passport-setup')
-const passport = require('passport')
-initializePassport(passport, email => {
-    return users.find(user => user.email === email)
-})
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+const db = require("./queries");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const flash = require("express-flash");
+const session = require("express-session");
+const initializePassport = require("./passport-setup");
+const passport = require("passport");
+initializePassport(passport, (email) => {
+  return users.find((user) => user.email === email);
+});
 
-require('dotenv').config()
+require("dotenv").config();
 
 let users = [];
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(
-    bodyParser.urlencoded({
-        extended: true,
-    })
-)
-app.use(express.urlencoded({ extended: false }))
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use(flash());
-app.use(session({
+// console.log("process.env: ", process.env);
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}));
-app.use(passport.initialize())
-app.use(passport.session())
+    saveUninitialized: false,
+    cookie: { secure: true },
+  })
+);
 
-app.get('/', (request, response) => {
-    response.json({ info: 'Node.js, Express, and Postgres API' })
-})
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/getStatuses', db.getStatuses)
-app.get('/getStatusesById', db.getStatusesById)
-app.get('/getBooks', db.getBooks)
-app.post('/createBook', db.createBook)
-app.post('/signup', async(req, res) => {
-    console.log('req: ', req);
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        })
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, and Postgres API" });
+});
 
-    } catch (error) {
-        console.log('error: ', error);
-    }
-    console.log('user: ', users);
+app.get("/getStatuses", db.getStatuses);
+app.get("/getStatusesById", db.getStatusesById);
+app.get("/getBooks", db.getBooks);
+app.post("/createBook", db.createBook);
+app.post("/signup", async (req, res) => {
+  console.log("req: ", req);
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    console.log("error: ", error);
+  }
+  console.log("user: ", users);
+});
+app.post("/login", async (req, res) => {
+  console.log("req: ", req);
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    console.log("error: ", error);
+  }
 
-})
-app.post('/login', async(req, res) => {
-    console.log('req: ', req);
-    try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.username,
-            email: req.body.email,
-            password: hashedPassword
-        })
-
-    } catch (error) {
-        console.log('error: ', error);
-    }
-
-    console.log('user: ', users);
-
-})
+  console.log("user: ", users);
+});
 
 app.listen(port, () => {
-    console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
